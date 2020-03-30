39ed240c62f5ccedc527e645045abb04d721fd36
ABB <bayhanbayramoglu@gmail.com>
2020-03-30 12:16:18 +0300

    Bir kaç düzenleme yapıldı.

--- components/globalcomponents/About.vue
+++ components/globalcomponents/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about about-background">
     <div class="about-profile">
       <div class="profile-img">
         <img :src="aboutUs.profileImg" alt="">
@@ -12,11 +12,10 @@
     <div class="about-content">
       <p>
         {{ aboutUs.content }}
-        Lorem ipsum
       </p>
       <div class="imza">
         <h2> {{ aboutUs.ownername }} </h2>
-        <img width="100" :src="aboutUs.autograph" alt="">
+        <img :src="aboutUs.autograph" alt="Signature">
       </div>
     </div>
   </div>
@@ -31,73 +30,106 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-.about{
-color:white;
-background-color: rgba(0, 0, 0, .01);
-backdrop-filter: blur(15px);
--webkit-backdrop-filter: blur(10px);
-width: 100%;
-height: 70vh;
-padding: 2rem;
-margin-top: 4rem;
-border-radius: 1em;
-font-family: Arial, sans-serif;
+  @supports (backdrop-filter: none) {
+    .about-background {
+      background-color: rgba(0, 0, 0, .01);
+      backdrop-filter: blur(15px);
+    }
+  }
+/* Tarayıcı desteği olmadığında seçilecek resim buraya gelecek.*/
+  @supports not (backdrop-filter: none) {
+    .about-background {
+      background-image: red;
+    }
+  }
+
+  .about {
+    color: white;
+    width: 100%;
+    height: 70vh;
+    padding: 2rem;
+    margin-top: 4rem;
+    border-radius: 1em;
+    font-family: Arial, sans-serif;
+  }
+
+  /* Create three equal columns that floats next to each other */
+  .about-profile {
+    float: left;
+    text-align: center;
+    width: 22%;
+    height: 100%;
+
+    h2 {
+      font-size: 1.6rem;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+    }
+
+    span {
+      font-size: 1rem;
+      padding: 1rem;
+    }
+
+    p {
+      font-size: 1rem;
+      padding-top: .5rem;
+    }
+
+    .profile-img img {
+      width: 100%;
+      border-radius: 10%;
+    }
+  }
+
+  .about-content {
+    height: 100%;
+    column-count: 2;
+    column-rule-style: solid;
+    column-rule-width: 1px;
+    padding: 1rem;
+    overflow: hidden;
+
+    p {
+      padding-right: 2rem;
+      padding-left: 2rem;
+      font: menu;
+      font-size: 1.06rem;
+    }
+
+  }
+
+  .imza {
+    padding-right: 3rem;
+    padding-left: 2rem;
+    text-align: right;
+    padding-top: 3rem;
+
+    h2 {
+      font-size: 2rem;
+    }
+
+    img {
+      width: 30%;
+      padding-top: 2rem;
+    }
+  }
+
+  #style-4::-webkit-scrollbar-track
+{
+    box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: #F5F5F5;
 }
 
-/* Create three equal columns that floats next to each other */
-.about-profile {
+#style-4::-webkit-scrollbar
+{
+    width: 10px;
+    background-color: #F5F5F5;
+}
 
-float: left;
-text-align: center;
-width: 22%;
-height: 100%;
-h2 {
-font-size: 1.6rem;
-padding-top: 1rem;
-padding-bottom: 1rem;
-}
-span {
-font-size: 1rem;
-padding: 1rem;
-}
-p {
-  font-size: 1rem;
-padding-top: .5rem;
-}
-.profile-img img {
-width: 100%;
-border-radius: 10%;
-}
-}
-.about-content {
-column-count: 2;
--webkit-column-count: 2;
--moz-column-count: 2;
-column-rule-style: solid;
--webkit-column-rule-style: solid;
--moz-column-rule-style: solid;
-column-rule-width: 1px;
--webkit-column-rule-width: 1px;
--moz-column-rule-width: 1px;
-padding: 1rem;
-p{
-
-padding-right: 2rem;
-padding-left: 2rem;
-font: menu;
-font-size: 1.06rem;
-}
-overflow: hidden;
-}
-.imza{
-padding-left: 6rem;
-text-align: center;
-padding-top: 3rem;
-h2{
-font-size: 2rem;
-}
-img{
-padding-top: 2rem;
-}
+#style-4::-webkit-scrollbar-thumb
+{
+    background-color: #000000;
+    border: 2px solid #555555;
 }
 </style>
