acc317ce986b3ab7c4ec8d3a88d8ddd9e70e6e69
serkankorkac <korkacserkan@gmail.com>
2020-04-07 12:09:26 +0300

    aa

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -6,11 +6,9 @@
 </template>
 <script>
 import Product from '@/components/products/Product'
-import Slide from '@/components/products/ProductSlide'
 export default {
   components: {
-    Product,
-    Slide
+    Product
   }
 }
 </script>
