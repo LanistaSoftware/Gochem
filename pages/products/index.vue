42979371c85c3b97908c3f6efb4ecbbb3c724b6e
serkankorkac <korkacserkan@gmail.com>
2020-04-07 11:13:24 +0300

    jj

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="cadfsard">
+    <Slide />
     <Product />
   </div>
 </template>
 <script>
 import Product from '@/components/products/Product'
+import Slide from '@/components/products/ProductSlide'
 export default {
   components: {
-    Product
+    Product,
+    Slide
   }
 }
 </script>
