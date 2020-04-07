b2a33477464336c3681ae974d7195120e7a0d0a7
serkankorkac <korkacserkan@gmail.com>
2020-04-07 18:37:28 +0300

    product carousel

--- components/products/ProductSlide.vue
+++ components/products/ProductSlide.vue
@@ -1,28 +1,19 @@
 <template>
-  <!-- <carousel-3d
-    class="carousel-product"
-    :controls-visible="true"
-    :controls-prev-html="'&#10093;'"
-    :controls-next-html="'&#10093;'"
-    :controls-width="20"
-    :controls-height="30"
-    :clickable="false"
-  >
-    <slide class="slider" v-for="(slide, i) in 5" :key="i" :index="i">
-      <div>Serkan</div>
-    </slide>
-  </carousel-3d> -->
-  <carousel
-    :per-page-custom="[[200, 3], [1024, 3]]"
-    :navigation-enabled="true"
-    :navigation-next-label="nexticon"
-    :navigation-click-target-size="2"
-    :pagination-enabled="false"
-  >
-    <slide v-for="(slide,index) in 5" :key="index" class="slider">
-      {{ index }}
-    </slide>
-  </carousel>
+  <div class="product-responsive-carousel">
+    <carousel
+      class="carousel-product"
+      :per-page-custom="[[200, 2.5], [1024, 3]]"
+      :navigation-enabled="true"
+      :navigation-next-label="nexticon"
+      :navigation-prev-label="previcon"
+      :navigation-click-target-size="2"
+      :pagination-enabled="false"
+    >
+      <slide v-for="(slide,index) in 5" :key="index" class="carousel-responsive">
+        Slide 1 Content
+      </slide>
+    </carousel>
+  </div>
 </template>
 <script>
 import { Carousel, Slide } from 'vue-carousel'
@@ -30,23 +21,39 @@ export default {
   components: { Carousel, Slide },
   data () {
     return {
-      nexticon: '<span class="next sl" style="color:red"><i class="flaticon-right"></i></span>'
+      nexticon: '<div class="cl-prev-next"><i class="flaticon-next"></i></div>',
+      previcon: '<div class="cl-prev-next"><i class="flaticon-back"></i></div>'
     }
   }
 }
 </script>
 <style lang="less" scoped>
-.slider{
+ .product-responsive-carousel{
+   width: 100%;
+   height: 50%;
+   margin-top:2rem;
+   border-radius:10px ;
    background-color:rgba(0, 0, 0, 0.2);
+ }
+.carousel-product{
+   margin-left: 18rem;
+   margin-right: 30rem !important;
+   color: white !important;
    border: none;
-    height: 20rem;
-    color: white;
-    opacity: 0.5;
+   text-align: center;
+   height: 9rem;
+   width: 50%;
+   border-radius:10px ;
+   font-size: 10px;
 }
-.VueCarousel-navigation-prev{
-    color: red !important;
-}
-.sl{
-    font-size: 20rem;
-}
+  .product-sidenav{
+    display: none;
+  }
+  .carousel-responsive:not(:last-child){
+    border-right: 1px solid white;
+ }
+ .carousel-responsive{
+   margin-top: 3rem;
+ }
+
 </style>
